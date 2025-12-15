6bac748258e67f47b92ddb04a8207fe296a28e29
Antoine-briz <antoine.brizard@outlook.fr>
2025-12-15 20:57:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -974,10 +974,12 @@ if (pos === "Décubitus latéral") {
 
     <div>
       Intubation par sonde double lumière :
-      <span class="img-link" onclick="openImg('uniplum1.png')">
+      <span class="img-link" onclick="openImg('iotdoublelum.png')">
         🖼️ Gestion de l’intubation
       </span>,
-      🖼️ Gestion de la ventilation uni-pulmonaire
+      <span class="img-link" onclick="openImg('gestionunipulm.png')">
+        🖼️ Gestion ventilation unipulmonaire
+      </span>,
     </div>
 
     <div style="margin-top:.5rem;">
@@ -995,7 +997,7 @@ if (pos === "Décubitus ventral") {
 
     <div>
       Procédure de décubitus ventral :
-      <span class="img-link" onclick="openImg('vdp.png')">
+      <span class="img-link" onclick="openImg('vd.png')">
         🖼️ Décubitus ventral
       </span>
     </div>
