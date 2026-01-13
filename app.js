0ed824fbc8e211049f3fc28564d6708b2792c019
Antoine-briz <antoine.brizard@outlook.fr>
2026-01-13 10:12:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17573,6 +17573,15 @@ async function ensureEnsAdminCodeOnce() {
 ========================= */
 
 function renderEnseignement() {
+
+  const esc = (s) =>
+    String(s ?? "")
+      .replaceAll("&", "&amp;")
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replaceAll('"', "&quot;")
+      .replaceAll("'", "&#039;");
+  
   const DOMAINS = [
     "Réanimation",
     "Anesthésie",
